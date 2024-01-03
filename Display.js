81269a1cbdbaaa27fdb1234b2915fb7da044df34
piaolivera <piaoliveraa@gmail.com>
2024-01-03 19:50:54 -0300

    proyecto calculadora con html, css y js

---
+++ Display.js
@@ -0,0 +1,19 @@
+class Display{
+    constructor(displayValorAnterior, displayValorActual) {
+        this.displayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculadora = new Calculadora();
+        this.valorActual = '';
+        this.valorAnterior = '';
+    }
+    agregarNumero(numero){
+        if(numero === '.' && this.valorActual.includes('.')) return
+        this.valorActual = this.valorActual.toString() + numero.toString();
+        this.imprimirValores();
+    }
+
+    imprimirValores(){
+        this.displayValorActual.textContent = this.valorActual;
+        this.valorAnterior.textContent = this.valorAnterior;
+    }
+}
